357e9c9104d963b75690c346a6c1e15e5c77af33
﻿Mathias Østby <maostby@uio.no>
2020-10-10 16:07:13 +0200

    Update AspFactorPrefix.java

--- no/uio/ifi/asp/parser/AspFactorPrefix.java
+++ no/uio/ifi/asp/parser/AspFactorPrefix.java
@@ -1,1 +1,31 @@
+package no.uio.ifi.asp.parser;
 
+import java.util.ArrayList;
+
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.runtime.*;
+import no.uio.ifi.asp.scanner.*;
+import static no.uio.ifi.asp.scanner.TokenKind.*;
+
+class AspFactorPrefix extends AspSyntax {
+  TokenKind kind;
+
+  AspFactorPrefix(int n) {
+    super(n);
+  }
+
+  static AspFactorPrefix parse(Scanner s) {
+    AspFactorPrefix afp = AspFactorPrefix(s.curLineNum());
+    enterParser("factor prefix");
+
+    test(s, TokenKind.plusToken, TokenKind.minusToken)
+    afp.kind = s.curToken().kind
+    
+    leaveParser("factor prefix")
+    return afp;
+  }
+
+  @Override
+  public void prettyPrint() {
+    //-- Must be changed in part 2:
+  }
